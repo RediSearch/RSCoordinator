4f761bdf2390703dc2299c137d1bcca6eec404fd
Dvir Volk <dvirsky@gmail.com>
2017-04-13 23:27:05 +0300

    moved to multiconn experiment

--- src/dep/rmr/cluster.c
+++ src/dep/rmr/cluster.c
@@ -70,7 +70,7 @@ MRCluster *MR_NewCluster(MRClusterTopology *initialTopolgy, ShardFunc sf,
   cl->topo = initialTopolgy;
   cl->nodeMap = NULL;
   cl->myNode = NULL;  // tODO: discover local ip/port
-  MRConnManager_Init(&cl->mgr);
+  MRConnManager_Init(&cl->mgr, 4);
 
   if (cl->topo) {
     _MRClsuter_UpdateNodes(cl);

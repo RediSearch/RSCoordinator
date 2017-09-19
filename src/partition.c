7ed52eed0d306925c9716c1d417f002402542495
Dvir Volk <dvirsky@gmail.com>
2017-09-19 18:19:55 +0300

    added separate partitioning file

---
+++ src/partition.c
@@ -0,0 +1,30 @@
+#include "partition.h"
+#include "fnv.h"
+
+size_t PartitionForKey(PartitionCtx *ctx, const char *key, size_t len) {
+  return fnv_32a_buf((void *)key, len, 0) % ctx->size;
+}
+
+const char *PartitionTag(PartitionCtx *ctx, size_t partition) {
+
+  if (partition > ctx->size) {
+    return NULL;
+  }
+
+  size_t step = ctx->tableSize / ctx->size;
+  // printf("parition %d, index %d\n", partition, partition * (sp->tableSize / sp->size));
+  return ctx->table[((partition + 1) * step - 1) % ctx->tableSize];
+}
+
+void PartitionCtx_Init(PartitionCtx *ctx, size_t numPartitions, const char **table,
+                       size_t tableSize) {
+  printf("Initializing partition to table %p size %d\n", table, tableSize);
+  ctx->size = numPartitions;
+  ctx->table = table;
+  ctx->tableSize = tableSize;
+}
+
+void PartitionCtx_SetSlotTable(PartitionCtx *ctx, const char **table, size_t tableSize) {
+  ctx->table = table;
+  ctx->tableSize = tableSize;
+}

46ff3144002a264f0065193fadc7a5a928002a80
Dvir Volk <dvirsky@gmail.com>
2017-11-05 17:35:47 +0200

    Version bump - 0.91.00 (9100)

--- src/version.h
+++ src/version.h
@@ -1,6 +1,13 @@
 #ifndef RSCOORDINATOR_VERSION_H_
 #define RSCOORDINATOR_VERSION_H_
 
-#define RSCOODINATOR_VERSION 9006
+#define RSCOODINATOR_VERSION_MAJOR 0
+#define RSCOODINATOR_VERSION_MINOR 91
+#define RSCOODINATOR_VERSION_PATCH 0
+
+// convert semver to incremental number as expected by redis
+#define RSCOODINATOR_VERSION                                               \
+  (RSCOODINATOR_VERSION_MAJOR * 10000 + RSCOODINATOR_VERSION_MINOR * 100 + \
+   RSCOODINATOR_VERSION_PATCH)
 
 #endif

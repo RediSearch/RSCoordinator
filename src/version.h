b46ed0668edba1d1bc41239dbb741360982c3e5b
Dvir Volk <dvirsky@gmail.com>
2018-02-08 22:21:29 +0200

    version 1.0.15 - prevent crash when aborting not iterator with no child

--- src/version.h
+++ src/version.h
@@ -3,7 +3,7 @@
 
 #define RSCOORDINATOR_VERSION_MAJOR 1
 #define RSCOORDINATOR_VERSION_MINOR 0
-#define RSCOORDINATOR_VERSION_PATCH 14
+#define RSCOORDINATOR_VERSION_PATCH 15
 
 // convert semver to incremental number as expected by redis
 #define RSCOORDINATOR_VERSION                                                \

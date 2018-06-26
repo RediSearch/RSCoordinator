f0ddfc1e0cac48884a4fafd3e91463638a10d4dc
Mark Nunberg <mnunberg@haskalah.org>
2018-06-26 15:05:46 -0400

    Fix constant name

--- src/module.c
+++ src/module.c
@@ -1047,7 +1047,7 @@ RedisModule_OnLoad(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
   }
 
   // Init the aggregation thread pool
-  DIST_AGG_THREADPOOL = ConcurrentSearch_CreatePool(CONCURRENT_SEARCH_POOL_SIZE);
+  DIST_AGG_THREADPOOL = ConcurrentSearch_CreatePool(CONCURRENT_SEARCH_POOL_DEFAULT_SIZE);
 
   /*********************************************************
    * Single-shard simple commands

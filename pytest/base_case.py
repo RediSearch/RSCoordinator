f8c5b0b8463886f49e6e276949a5f59444ed6dd3
Mark Nunberg <mnunberg@haskalah.org>
2018-07-18 11:41:42 -0400

    Modify base test to run with OSS tests

--- pytest/base_case.py
+++ pytest/base_case.py
@@ -1,31 +1,22 @@
-from rmtest.cluster import ClusterModuleTestCase
-import redis
-import unittest
-from contextlib import contextmanager
-import time
+import rmtest.config
+from oss_tests.base_case import FTBaseCaseMethods
 
 
-class BaseSearchTestCase(ClusterModuleTestCase('../src/module-oss.so',
-                                               num_nodes=3, module_args=['PARTITIONS', 'AUTO'])):
+class BaseSearchTestCase(rmtest.ClusterTestCase, FTBaseCaseMethods):
+    @classmethod
+    def get_module_args(cls):
+        print("Getting module args!!!")
+        return super(BaseSearchTestCase, cls).get_module_args() + ['PARTITIONS', 'AUTO']
 
     def setUp(self):
         # Update all the nodes
-        self.client().execute_command('ft.clusterrefresh')
-        time.sleep(0.5)
+        r = self.client
+        r.execute_command('ft.clusterrefresh')
+        # time.sleep(0.5)
         self.broadcast('ft.clusterrefresh')
 
-        self.flushdb()
-
-    def search(self, *args):
-        return self.client().execute_command('ft.search', *args)
-
     def broadcast(self, cmd, *args):
-        return self.client().execute_command('ft.broadcast', cmd, *args)
+        return self.client.execute_command('ft.broadcast', cmd, *args)
 
     def flushdb(self):
         self.broadcast('flushdb')
-
-    @contextmanager
-    def redis(self):
-
-        yield self.client()

9749f43258ab61f61ad72355a1391957ee3c9633
meir@redislabs.com <meir@redislabs.com>
2020-01-28 15:20:27 +0200

    upgrade version to 1.4.25

--- src/version.h
+++ src/version.h
@@ -4,7 +4,7 @@
 
 #define RSCOORDINATOR_VERSION_MAJOR 1
 #define RSCOORDINATOR_VERSION_MINOR 4
-#define RSCOORDINATOR_VERSION_PATCH 24
+#define RSCOORDINATOR_VERSION_PATCH 25
 
 // convert semver to incremental number as expected by redis
 #define RSCOORDINATOR_VERSION                                                \

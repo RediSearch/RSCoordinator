4eac6a13b061f5955c3127b363b8325c6dc4da29
Rafi Einstein <rafi@redislabs.com>
2020-03-26 20:18:31 +0200

    Testing environment fixes (#118) (#125)

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
+
 set -x
 set -e
 
+HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+ROOT=$(readlink -f $HERE/..)
+
 MODULE_OSS_SO=$BUILD_DIR/module-oss.so
 
-if [ -n "$REDISEARCH_CI_SKIP_TESTS" ]; then
+if [[ -n "$REDISEARCH_CI_SKIP_TESTS" ]]; then
     exit 0
 fi
 
-ROOT=$PWD
 cd $BUILD_DIR
 
 cat > rmtest.config << EOF
@@ -17,5 +20,5 @@ module = $ROOT/$MODULE_OSS_SO
 EOF
 
 ctest -V
-RLTest --env oss-cluster --env-reuse --t $ROOT/src/dep/RediSearch/src/pytest/ --clear-logs --shards-count 3 --module $ROOT/$MODULE_OSS_SO --module-args "PARTITIONS AUTO"
-RLTest --env oss-cluster --env-reuse --t $ROOT/src/dep/RediSearch/src/pytest/ --clear-logs --shards-count 3 --module $ROOT/$MODULE_OSS_SO --module-args "PARTITIONS AUTO SAFEMODE"
+python -m RLTest --env oss-cluster --env-reuse -t $ROOT/src/dep/RediSearch/src/pytest/ --clear-logs --shards-count 3 --module $ROOT/$MODULE_OSS_SO --module-args "PARTITIONS AUTO"
+python -m RLTest --env oss-cluster --env-reuse -t $ROOT/src/dep/RediSearch/src/pytest/ --clear-logs --shards-count 3 --module $ROOT/$MODULE_OSS_SO --module-args "PARTITIONS AUTO SAFEMODE"

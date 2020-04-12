89974445891517976030e7116507da93c7b3387c
Meir Shpilraien (Spielrein) <meir@redislabs.com>
2020-04-12 14:24:34 +0300

    update version to 1.6.12 (#127)
    * update version to 1.6.12
    
    * update RediSearch submodule

--- src/version.h
+++ src/version.h
@@ -4,7 +4,7 @@
 
 #define RSCOORDINATOR_VERSION_MAJOR 1
 #define RSCOORDINATOR_VERSION_MINOR 6
-#define RSCOORDINATOR_VERSION_PATCH 11
+#define RSCOORDINATOR_VERSION_PATCH 12
 
 // convert semver to incremental number as expected by redis
 #define RSCOORDINATOR_VERSION                                                \

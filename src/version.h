9dc68863c810772265b061dcb23a56a2f21adc68
meir@redislabs.com <meir@redislabs.com>
2020-04-20 13:17:54 +0300

    update version to 1.8.0

--- src/version.h
+++ src/version.h
@@ -3,8 +3,8 @@
 #include "dep/RediSearch/src/version.h"
 
 #define RSCOORDINATOR_VERSION_MAJOR 1
-#define RSCOORDINATOR_VERSION_MINOR 6
-#define RSCOORDINATOR_VERSION_PATCH 12
+#define RSCOORDINATOR_VERSION_MINOR 8
+#define RSCOORDINATOR_VERSION_PATCH 0
 
 // convert semver to incremental number as expected by redis
 #define RSCOORDINATOR_VERSION                                                \

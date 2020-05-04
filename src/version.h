70d5c55070a1c35a13890366d4a316a44b4d7651
Meir Shpilraien (Spielrein) <meir@redislabs.com>
2020-05-04 10:32:14 +0300

    update version to 1.4.28 (#130)

--- src/version.h
+++ src/version.h
@@ -4,7 +4,7 @@
 
 #define RSCOORDINATOR_VERSION_MAJOR 1
 #define RSCOORDINATOR_VERSION_MINOR 4
-#define RSCOORDINATOR_VERSION_PATCH 27
+#define RSCOORDINATOR_VERSION_PATCH 28
 
 // convert semver to incremental number as expected by redis
 #define RSCOORDINATOR_VERSION                                                \

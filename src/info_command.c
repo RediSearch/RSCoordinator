522fd458fafc0c6be81dc99cabbb8a2dfa132cd8
rafie <rafi@redislabs.com>
2020-07-26 18:00:07 +0300

    [WIP] ft.create async indexing

--- src/info_command.c
+++ src/info_command.c
@@ -26,7 +26,9 @@ static InfoFieldSpec toplevelSpecs_g[] = {
     {.name = "records_per_doc_avg", .type = InfoField_DoubleAverage},
     {.name = "bytes_per_record_avg", .type = InfoField_DoubleAverage},
     {.name = "offsets_per_term_avg", .type = InfoField_DoubleAverage},
-    {.name = "offset_bits_per_record_avg", .type = InfoField_DoubleAverage}};
+    {.name = "offset_bits_per_record_avg", .type = InfoField_DoubleAverage},
+    {.name = "indexing", .type = InfoField_WholeSum},
+    {.name = "percent_indexed", .type = InfoField_WholeSum}};
 
 static InfoFieldSpec gcSpecs[] = {
     {.name = "current_hz", .type = InfoField_DoubleAverage},

4f63b753313a27905b8e113570f420afbf4f5aa2
Meir Shpilraien (Spielrein) <meir@redislabs.com>
2020-08-31 14:16:32 +0300

    update version to 1.8.3 (#157)
    * update version to 1.8.3
    
    * update redisearch submodule to latest
    
    * update redisearch to 1.8 latest

--- src/version.h
+++ src/version.h
@@ -4,7 +4,7 @@
 
 #define RSCOORDINATOR_VERSION_MAJOR 1
 #define RSCOORDINATOR_VERSION_MINOR 8
-#define RSCOORDINATOR_VERSION_PATCH 2
+#define RSCOORDINATOR_VERSION_PATCH 3
 
 // convert semver to incremental number as expected by redis
 #define RSCOORDINATOR_VERSION                                                \

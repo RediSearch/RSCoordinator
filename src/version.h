44a55cda7ed6a55e7d93a314edf370fafe57d71f
rafie <rafi@redislabs.com>
2020-10-01 15:13:31 +0300

    Updated RediSearch, updated version to 2.0.1

--- src/version.h
+++ src/version.h
@@ -4,7 +4,7 @@
 
 #define RSCOORDINATOR_VERSION_MAJOR 2
 #define RSCOORDINATOR_VERSION_MINOR 0
-#define RSCOORDINATOR_VERSION_PATCH 0
+#define RSCOORDINATOR_VERSION_PATCH 1
 
 // convert semver to incremental number as expected by redis
 #define RSCOORDINATOR_VERSION                                                \

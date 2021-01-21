e6d5fac676fe7cb5ed58f21d1ce1552ced4716d7
ariel shtul <ashtul@gmail.com>
2021-01-21 17:50:05 +0200

    bump version

--- src/version.h
+++ src/version.h
@@ -4,7 +4,7 @@
 
 #define RSCOORDINATOR_VERSION_MAJOR 1
 #define RSCOORDINATOR_VERSION_MINOR 6
-#define RSCOORDINATOR_VERSION_PATCH 14
+#define RSCOORDINATOR_VERSION_PATCH 15
 
 // convert semver to incremental number as expected by redis
 #define RSCOORDINATOR_VERSION                                                \

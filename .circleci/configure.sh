2c52c3e970e9e8530feb5244edfb7ef50a1c18b5
meir@redislabs.com <meir@redislabs.com>
2021-05-31 15:46:06 +0300

    apply hiredis patch on circle run

--- .circleci/configure.sh
+++ .circleci/configure.sh
@@ -3,6 +3,9 @@ set -x
 set -e
 
 PROJECT_ROOT=$PWD
+
+pushd $PROJECT_ROOT/src/dep/rmr/hiredis/;git apply $PROJECT_ROOT/hiredis_patch;touch applied_hiredis_patch;popd;
+
 mkdir -p $BUILD_DIR
 cd $BUILD_DIR
 

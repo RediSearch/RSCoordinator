56a2500526f7cda919e6399bf6158bbad53b4e21
rafie <rafi@redislabs.com>
2021-06-21 20:50:12 +0300

    fixes 1

--- .circleci/package.sh
+++ .circleci/package.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-set -x
-set -e
+set -xe
 
 HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-ROOT=$(cd $HERE/..; pwd)
+ROOT=$(cd $HERE/.. && pwd)
 
 BUILD_DIR=${BUILD_DIR:-build}
+
+mkdir -p $ROOT/$BUILD_DIR
+BUILD_DIR=$(cd $ROOT/$BUILD_DIR; pwd)
 
 export ARTDIR=$ROOT/artifacts
 export PACKAGE_NAME=redisearch
 export RAMP_YAML=$ROOT/ramp.yml
 
-$ROOT/src/dep/RediSearch/pack.sh $ROOT/$BUILD_DIR/module-enterprise.so
+$ROOT/src/dep/RediSearch/pack.sh $BUILD_DIR/module-enterprise.so
